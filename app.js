4bdd95a20e141318e56c223cd7347747853819a3
claudio <contacto.claudio93@gmail.com>
2024-03-04 18:10:09 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener('mousemove', moveCursor);
+
+function moveCursor(e) {
+    let x = e.clientX
+    let y = e.clientY
+
+    innerCursor.style.left = `${x}px`;
+    innerCursor.style.top = `${y}px`;
+    outerCursor.style.left = `${x}px`;
+    outerCursor.style.top = `${y}px`;
+}
+
+let link = Array.from(document.querySelectorAll('a'));
+
+/* link.forEach((link) => {
+    link.addEventListener('mouseover', ()=>{
+        innerCursor.classList.add('grow');
+    })
+    link.addEventListener('mouseleave', ()=>{
+        innerCursor.classList.remove('grow');
+    })
+}) */
+
+link.forEach((link) => {
+    link.addEventListener('mouseover', ()=>{
+        outerCursor.classList.add('grow');
+    })
+    link.addEventListener('mouseleave', ()=>{
+        outerCursor.classList.remove('grow');
+    })
+})
+
+
