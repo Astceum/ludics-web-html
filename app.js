a45a62c61151bf90e1dc7da585a4fc44efb90c2f
claudio <contacto.claudio93@gmail.com>
2024-06-04 14:02:20 -0400

    video main

--- app.js
+++ app.js
@@ -145,13 +145,6 @@ function toggleLogo() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  var video = document.getElementById('video-estrellas');
-  video.muted = true;
-  video.play().catch(function(error) {
-      console.log('Autoplay was prevented:', error);
-  });
-});
 
 let animacionActiva = false;
 
